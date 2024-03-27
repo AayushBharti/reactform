9dacb2cd48a2f3b9ce0bd621e8b8022410ef4261
Aayush Bharti <aayushbh2002@gmail.com>
2024-03-27 18:31:27 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // const [firstName, setFirstName] = new useState("");
+  // const [lastName, setLastName] = new useState("");
+
+  // function changeFirstNameHandler(e) {
+  //   // console.log(e.target.value);
+  //   setFirstName(e.target.value);
+  // }
+
+  // function changeLastNameHandler(e) {
+  //   // console.log(e.target.value);
+  //   setLastName(e.target.value);
+  // }
+
+  const [formData, setFormData] = useState({ firstName: "", lastName: "",email: "" });
+  // console.log(formData.email)
+
+  function changeHandler(e) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData, //copying remaining feilds as it is
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="">
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={changeHandler}
+          name="firstName"
+        />
+        <br/>
+        <br/>
+        <br/>
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={changeHandler}
+          name="lastName"
+        />
+        <br/>
+        <br/>
+        <br/>
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          onChange={changeHandler}
+          name="email"
+        />
+      </form>
     </div>
   );
 }
